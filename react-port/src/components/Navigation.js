cf280b9662a0f0d20cb3d2faf7e5347b16b3f3ee
KT <132851569+kumih0@users.noreply.github.com>
2024-01-09 11:27:22 -0600

    remembering how to import/export components

--- react-port/src/components/Navigation.js
+++ react-port/src/components/Navigation.js
@@ -1,8 +1,16 @@
-<nav>
-    <ul>
-        <li><a href='#'>Home</a></li>
-        <li><a href='#'>About</a></li>
-        <li><a href='#'>Projects</a></li>
-        <li><a href='#'>Contact</a></li>
-    </ul>
-</nav>
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            <ul className='nav-items'>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>About</a></li>
+                <li><a href='#'>Projects</a></li>
+                <li><a href='#'>Contact</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
