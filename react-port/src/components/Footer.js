7be311fda830a591032af13e2527b14b8be1c884
KT <132851569+kumih0@users.noreply.github.com>
2024-01-09 13:11:44 -0600

    importing bootstrap and bootstrap icons

---
+++ react-port/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+        <footer id="footer" className="footer">
+            <div class="container">
+                <div class="socials-media">
+
+                    <ul>
+                        <li><a href="#"><i class="bi bi-github"></i></a></li>
+                        <li><a href="#"><i class="bi bi-linkedin"></i></a></li>
+                        <li><a href="#"><i class="bi bi-youtube"></i></a></li>
+                        <li><a href="#"><i class="bi bi-instagram"></i></a></li>
+                    </ul>
+
+                </div>
+            </div>
+        </footer>
+    );
+};
