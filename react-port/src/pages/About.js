a63f368bce9ebe0c42437c21f64039cd0d8452b1
KT <132851569+kumih0@users.noreply.github.com>
2024-01-13 11:13:19 -0600

    correct imports and utilize rxt bs builtins

--- react-port/src/pages/About.js
+++ react-port/src/pages/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Container from 'react-bootstrap/esm/Container';
-import Row from 'react-bootstrap/esm/Row';
-import Col from 'react-bootstrap/esm/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const About = () => {
     return (
--- react-port/src/pages/Resume.js
+++ react-port/src/pages/Resume.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import Container from 'react-bootstrap/esm/Container';
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Stack from 'react-bootstrap/Stack';
 import Accordion from 'react-bootstrap/Accordion';
-import AccordionItem from 'react-bootstrap/esm/AccordionItem';
+import AccordionItem from 'react-bootstrap/AccordionItem';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
 
 function Resume() {
     return (
@@ -12,41 +15,37 @@ function Resume() {
             <Container fluid >
                 <Stack gap={3}>
                     <Accordion defaultActiveKey="0">
+
                         <Row className="work-exp">
                             <Col>
-                            <AccordionItem eventKey="0">
-                                <Accordion.Header className="title-heading">
-                                    <h3>Experience</h3>
-                                </Accordion.Header>
-                                <Accordion.Body className="work-exp-item">
-                                    <Col>
-                                        <Row className="job-heading">
-                                            <Col className="job-place">
-                                                <h4 className="title-heading">Job Title</h4>
-                                                <h5 className="subtitle">Place, location</h5>
-                                            </Col>
-                                            <Col className="subtitle job-time">
-                                                <p className="description">
-                                                    Month Year - Month Year
-                                                </p>
-                                            </Col>
-                                        </Row>
-                                        
-                                        <Row className="job-detail">
-                                            <Col>
-                                                <p>Brief description of job</p>
-                                                <ul>
-                                                    <li>highlight 1</li>
-                                                    <li>highlight 2</li>
-                                                    <li>highlight 3</li>
-                                                </ul>
-                                            </Col>
-                                        </Row>
-                                    </Col>
-                                </Accordion.Body>
-                            </AccordionItem>
+                                <AccordionItem eventKey="0">
+                                    <Accordion.Header className="title-heading">
+                                        <h3>Experience</h3>
+                                    </Accordion.Header>
+                                    <Accordion.Body className="work-exp-items">
+
+                                        <Card>
+                                            <Card.Header className="job-heading">
+                                                <Card.Title className="job-title">Job Title</Card.Title>
+                                                <Card.Subtitle className="job-place">Place, Location</Card.Subtitle>
+                                                <Card.Text className="job-time">Month Year - Month Year</Card.Text>
+                                            </Card.Header>
+
+                                            <Card.Body>
+                                                <Card.Text className="job-detail">Brief description of job</Card.Text>
+                                            </Card.Body>
+                                            <ListGroup className="job-highlights">
+                                                <ListGroupItem>highlight 1</ListGroupItem>
+                                                <ListGroupItem>highlight 2</ListGroupItem>
+                                                <ListGroupItem>highlight 3</ListGroupItem>
+                                            </ListGroup>
+                                        </Card>
+
+                                    </Accordion.Body>
+                                </AccordionItem>
                             </Col>
                         </Row>
+
                         <Row className="edu">
                             <Col>
                                 <AccordionItem eventKey="1">
@@ -54,51 +53,63 @@ function Resume() {
                                         <h3>Education</h3>
                                     </Accordion.Header>
                                     <Accordion.Body className="detail">
-                                        <Col>
-                                            <h4>Yonsei University, Underwood International College</h4>
-                                        </Col>
-                                        <Col>
-                                            <p>Bachelor's of Arts, Asian Studies</p>
-                                        </Col>
-                                        <Col>
-                                            <p>Graduated 2019</p>
-                                        </Col>
+
+                                        <Card>
+                                            <Card.Title>Yonsei University, Underwood International College</Card.Title>
+                                            <Card.Subtitle>Bachelor's of Arts, Asian Studies</Card.Subtitle>
+                                            <Card.Text>Graduated 2019</Card.Text>
+                                        </Card>
+
+                                        <p>See also: Certifications for additional education.</p>
+
                                     </Accordion.Body>
                                 </AccordionItem>
                             </Col>
                         </Row>
+
                         <Row className="skills">
                             <Col>
                                 <AccordionItem eventKey="2">
                                     <Accordion.Header className="title-heading">
                                         <h3>Skills</h3>
                                     </Accordion.Header>
+
                                     <Accordion.Body>
-                                        <ul className="skills-list">
-                                            <li>relevant skill</li>
-                                            <li>skill 2</li>
-                                            <li>skill 3</li>
-                                        </ul>
+
+                                        <ListGroup horizontal="sm" className="skills-list">
+                                            <ListGroupItem>relevant skill</ListGroupItem>
+                                            <ListGroupItem>relevant skill 2</ListGroupItem>
+                                            <ListGroupItem>relevant skill 3</ListGroupItem>
+                                        </ListGroup>
+
                                     </Accordion.Body>
+
                                 </AccordionItem>
                             </Col>
                         </Row>
+
                         <Row className="certs">
                             <Col>
                                 <AccordionItem eventKey="3">
                                     <Accordion.Header className="title-heading">
                                         <h3>Certifications</h3>
                                     </Accordion.Header>
+
                                     <Accordion.Body>
-                                        <ul className="cert-list">
-                                            <li>certicate 1</li>
-                                            <li>cert ex 2</li>
-                                            <li>cert ex 3 date</li>
-                                        </ul>
+
+                                        <Card>
+                                            <Card.Title>Certification title</Card.Title>
+                                            <Card.Subtitle>date completed</Card.Subtitle>
+                                            <Card.Text>Institution</Card.Text>
+                                            <Card.Link href="#">Further info.</Card.Link>
+                                        </Card>
+
                                     </Accordion.Body>
+
                                 </AccordionItem>
                             </Col>
                         </Row>
+
                     </Accordion>
                 </Stack>
             </Container>
