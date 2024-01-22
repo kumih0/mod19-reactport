efd46e77c7de4349fb5212fe8da746d6dc0d02aa
KT <132851569+kumih0@users.noreply.github.com>
2024-01-21 18:54:53 -0600

    complete contact from layout, imgs for face/bckgd

--- react-port/src/pages/About.js
+++ react-port/src/pages/About.js
@@ -1,30 +1,23 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card'
 import CardTitle from 'react-bootstrap/CardTitle';
 import CardText from 'react-bootstrap/CardText';
 import CardImgOverlay from 'react-bootstrap/esm/CardImgOverlay';
+import face from '../assets/img/me/cottoncandy-face.JPG';
 
 
 const About = () => {
     return (
-    <div id="about" >
-        <Container>
-            <Row>
-                <Card>
-                    <CardImg />
-                    {/* will contain img carousel */}
-                    <CardImgOverlay>
-                        {/* will contain details as overlay */}
+            <Card id="about" >
+                <CardImg src={face} alt="Me :)" />
+                {/* will contain img carousel */}
+                <CardImgOverlay>
+                    {/* will contain details as overlay */}
                     <CardTitle>About Me</CardTitle>
                     <CardText>Hi it's me</CardText>
-                    </CardImgOverlay>
-                </Card>
-            </Row>
-        </Container>
-    </div>
+                    <CardText>More about me</CardText>
+                </CardImgOverlay>
+            </Card>
     );
 };
 
---
+++ react-port/src/pages/Contact.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
+import FormGroup from 'react-bootstrap/esm/FormGroup';
+import FloatingLabel from 'react-bootstrap/esm/FloatingLabel';
+
+const ContactForm = () => {
+    return (
+        <Form>
+            <FormGroup className="" controlId='contactEmail'>
+                <FloatingLabel controlId='email' label="Email address" className='mb-3'>
+                    <FormControl type="email" placeholder="Email address" required />
+                </FloatingLabel>
+            </FormGroup>
+            <FormGroup className="" controlId="contactName">
+                <FloatingLabel controlId="name" label="Contact name" className='mb-3'>
+                    <FormControl type="text" placeholder="Contact name" required />
+                </FloatingLabel>
+            </FormGroup>
+            <FormGroup className="" controlId="msgSubject">
+                <FloatingLabel controlId="subject" label="Subject" className='mb-3'>
+                    <FormControl type="text" placeholder="Subject" required />
+                </FloatingLabel>
+            </FormGroup>
+            <FormGroup className="" controlId='msgText'>
+                <FloatingLabel controlId="message" label="Message" required>
+                    <Form.Control
+                        as="textarea"
+                        placeholder="Leave a message here :)"
+                        style={{ height: '200px' }}
+                    />
+                </FloatingLabel>
+            </FormGroup>
+            <Button variant="primary" type="submit">Send message</Button>
+        </Form>
+    );
+};
