da3009f66af9a1253b80f282e8c0e3eb4970c60f
KT <132851569+kumih0@users.noreply.github.com>
2024-04-05 06:59:30 -0500

    checking if work and remove typing ani

--- react-port/src/index.js
+++ react-port/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './assets/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import App from './App';
--- react-port/src/pages/Home.js
+++ react-port/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Container from "react-bootstrap/esm/Container";
 import Card from 'react-bootstrap/Card'
-import Typing from 'react-typing-animation';
+// import Typing from '@types/react-typing-animation';
 import mainImg from '../assets/img/me/yeouido-uke.JPG';
 import CardImg from "react-bootstrap/esm/CardImg";
 import CardImgOverlay from "react-bootstrap/esm/CardImgOverlay";
@@ -15,25 +15,27 @@ const Home = () => {
             <Card id="home">
             <CardImg src={mainImg} fluid />
             <CardImgOverlay>
-                <Typing startDelay={100} speed={50}>
+                {/* <Typing startDelay={100} speed={50}> */}
                     <CardTitle id="greeting">
                         Hey there, I'm KT.
                     </CardTitle>
-                    <Typing.Delay ms={1000} />
-                    <Typing.Speed ms={200} />
+                    {/* <Typing.Delay ms={1000} />
+                    <Typing.Speed ms={200} /> */}
                     <CardSubtitle id="oneliner">
                         I'm a programmer, educator, baker, artist, and craft enthusiast.
                     </CardSubtitle>
-                    <Typing.Backspace count={20} />
+                    {/* <Typing.Backspace count={20} /> */}
                     <CardText id="ty">
                         Thanks for dropping by :)
                     </CardText>
-                    <Typing.Backspace count={20} />
+                    {/* <Typing.Backspace count={20} />
                     
-                </Typing>
+                </Typing> */}
                 {/* bs icons */}
             </CardImgOverlay>
             </Card>
         </Container>
     );
 }; 
+
+export default Home;
