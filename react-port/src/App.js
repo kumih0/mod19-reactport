4531c600e0e96c640475e8bb7cdfef1d22b6dcc4
KT <132851569+kumih0@users.noreply.github.com>
2024-04-09 04:53:44 -0500

    adding pages/comp to check how work

--- react-port/src/App.js
+++ react-port/src/App.js
@@ -1,11 +1,18 @@
 // import './assets/css/styles.css';
 import Home from './pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
       <Home></Home>
+      <About></About>
+      <Contact></Contact>
+      <Footer></Footer>
     </div>
   );
 }
