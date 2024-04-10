c0b9f4b5763abccf7503a14bf7fe4711526c9fc4
KT <132851569+kumih0@users.noreply.github.com>
2024-04-10 09:55:53 -0500

    simplify exports comp w index file

---
+++ react-port/src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Header } from './Header';
+export { default as Footer} from './Footer';
+export { default as NavBar } from './Navigation';
+export { default as ContactForm } from './ContactForm';
+export { default as Resume } from './Resume';
+export { default as Project } from './Project';
